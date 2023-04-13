4d2e885168469c604a5b472e1e1d355c2a14e561
samira garcia <samira.garcia95@gmail.com>
2023-04-13 18:09:10 -0300

    ejercicio 8

---
+++ ejercicio8.js
@@ -0,0 +1,14 @@
+console.log( 4>4);
+console.log( 23 == 33);
+console.log(37 >= -3);
+console.log("Joel" == "Joel");
+console.log("joel" == "Joel");
+console.log("s7829s;SA" != "123querty");
+console.log( ("joel" == "Joel") && ("s7829s;SA" == "s7829s;SA") );
+console.log( ("joel" == "Joel") || ("s7829s;SA" == "s7829s;SA") );
+console.log( ( "joel" == "Joel" ) && ("s7829s;SA" != "123querty"));
+console.log(18>=100 && 18<100);
+console.log( 18>=100 || 18<100);
+console.log( 18 <= 100 || 18>100);
+console.log( 18 > 100 && 18 <100);
+console.log( 18 > 100 || 18<100);
